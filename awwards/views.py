91b06ba354768f31d3b915eea17be76d353719c2
Akumucollins <akumucollins001@gmail.com>
2020-10-24 15:12:53 +0300

    created view fuctions for  home page, login and logout

--- awwards/views.py
+++ awwards/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, HttpResponseRedirect, redirect, get_object_or_404
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout as django_logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+# from .token_generator import account_activation_token
+def home(request):
+    return render(request,"home.html")
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect(request,'/')
+    
+    return render(request, '/django_registration/login.html')
+        
+@login_required
+def logout(request):
+    django_logout(request)
+    return  HttpResponseRedirect('/')
