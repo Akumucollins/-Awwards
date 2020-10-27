1bcd254b922b68dcdd7d4d55fcb5eef926ca0dc8
Akumucollins <akumucollins001@gmail.com>
2020-10-27 22:40:58 +0300

    added ratings and projects urlpatterns

--- awwards/urls.py
+++ awwards/urls.py
@@ -2,11 +2,19 @@ from  django.urls import path
 from . import views 
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
+
+# router = routers.DefaultRouter()
+# router.register(r'profile', views.ProfileViewSet)
+# router.register(r'project', views.ProjectViewSet)
+
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('logout/', views.logout, name='logout'),
-    path('profile/',views.profile, name='profile'),
+    path('new/profile/',views.new_profile, name='profile'),
+    path('project/', views.project, name='project'),
+    path('rating/<int:pk>/',views.rating,name='rating'),
 ]
 
 if settings.DEBUG:
